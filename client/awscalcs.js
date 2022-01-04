af7587424d5a5cc9deb8da2291debe78637795d0
aselunar <aselunar@gmail.com>
2022-01-03 20:22:48 -0800

    Working on awscalcs

--- client/awscalcs.js
+++ client/awscalcs.js
@@ -1,6 +1,35 @@
 //This is only us-east-1, so it is 184 MB.
+const awsPrices = require('../server/awsProduct.json');
+
 const urlForAwsUsEast = 'https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/us-east-1/index.json';
-const awsPrices = {};/* An object of every instanceType and pricePerUnit of the fetch request where vCPU and memory is greater or equal to the vCPU and RAM that the user inputted and operating system equals the operating system the user inputted. */
+
+let inputMem = 25;
+let inputVcpu = 1;
+
+awsPrices.forEach(obj => {
+    obj.vcpu = parseInt(obj.vcpu);
+    if (inputVcpu === obj.vcpu || )
+
+    if (obj.memory){
+        obj.memory = parseInt(obj.memory);
+        // obj.memory.replace(/\D/ig, "");
+
+    }
+    
+    obj.pricePerUnit = parseFloat(obj.pricePerUnit);
+    }
+);
+console.log(awsPrices[0]);
+
+let userInput
+
+const awsPricesForCustomer = 
+
+
+/* An object of every instanceType and pricePerUnit of the fetch request where vCPU and memory is greater or equal to the vCPU and RAM that the user inputted and operating system equals the operating system the user inputted. */
+
+
+
 const lowestAwsPrice = () => {
     const minPrice = {
         instanceType: '',
@@ -8,7 +37,7 @@ const lowestAwsPrice = () => {
     };
  for (let price in awsPrices){
      if (price.pricePerUnit < minPrice.pricePerUnit){
-        minPrice = price;
+        minPrice.pricePerUnit = price;
      }
  }
  return minPrice;
