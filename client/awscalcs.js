42e499cbbe5076256fdfc9764ee90c95a9502a64
aselunar <aselunar@gmail.com>
2022-01-04 07:00:28 -0800

    Finished awscalcs after the awsProduct.json

--- client/awscalcs.js
+++ client/awscalcs.js
@@ -1,15 +1,11 @@
 //This is only us-east-1, so it is 184 MB.
 const awsPrices = require('../server/awsProduct.json');
+import { useSelector } from 'react-redux';
 
-const urlForAwsUsEast = 'https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/us-east-1/index.json';
-
-let inputMem = 25;
-let inputVcpu = 1;
+// const urlForAwsUsEast = 'https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/AmazonEC2/current/us-east-1/index.json';
 
 awsPrices.forEach(obj => {
     obj.vcpu = parseInt(obj.vcpu);
-    if (inputVcpu === obj.vcpu || )
-
     if (obj.memory){
         obj.memory = parseInt(obj.memory);
         // obj.memory.replace(/\D/ig, "");
@@ -21,9 +17,12 @@ awsPrices.forEach(obj => {
 );
 console.log(awsPrices[0]);
 
-let userInput
+const { inputMem, inputVcpu, inputGBs, numOfVms, hoursPerMonth } = useSelector((state) => state);
 
-const awsPricesForCustomer = 
+
+// let inputMem = 25;
+// let inputVcpu = 1;
+
 
 
 /* An object of every instanceType and pricePerUnit of the fetch request where vCPU and memory is greater or equal to the vCPU and RAM that the user inputted and operating system equals the operating system the user inputted. */
@@ -31,22 +30,33 @@ const awsPricesForCustomer =
 
 
 const lowestAwsPrice = () => {
-    const minPrice = {
-        instanceType: '',
+//     const minPrice = {
+//         instanceType: '',
+//         pricePerUnit: Infinity,
+//     };
+//  for (let price in awsPrices){
+//      if (price.pricePerUnit < minPrice.pricePerUnit){
+//         minPrice.pricePerUnit = price;
+//      }
+//  }
+//  return minPrice;
+    const minPrice ={
         pricePerUnit: Infinity,
     };
- for (let price in awsPrices){
-     if (price.pricePerUnit < minPrice.pricePerUnit){
-        minPrice.pricePerUnit = price;
-     }
- }
- return minPrice;
+    awsPrices.forEach(obj => {
+
+        if (inputVcpu >= obj.vcpu && inputVcpu <= (obj.vcpu * 1.25) && inputMem >= obj.memory && inputMem <= (obj.memory * 1.25) && obj.pricePerUnit < minPrice.pricePerUnit){
+            minPrice = obj;
+        };
+    });
+    return minPrice;
+
 };
 
 const awsInstanceTypeToUse = lowestAwsPrice();
 
 //numofVMs and hoursPerMonth defined based on user inputs
-const awsVmSubTotal = awsInstanceTypeToUse.pricePerUnit * numOfVMs * hoursPerMonth;
+const awsVmSubTotal = awsInstanceTypeToUse.pricePerUnit * numOfVms * hoursPerMonth;
 
 //Good enough for minimum vialble product. 10 cents per GB. Source: https://calculator.aws/#/createCalculator/EC2
 const awsStorageSubTotal = inputGBs * 0.10 * numOfVMs;
@@ -54,4 +64,15 @@ const awsStorageSubTotal = inputGBs * 0.10 * numOfVMs;
 //Total per month
 const awsTotal = awsVmSubTotal + awsStorageSubTotal;
 
+const objToExport = {
+    instanceType: awsInstanceTypeToUse.instanceType,
+    vcpu: awsInstanceTypeToUse.vcpu,
+    memory: awsInstanceTypeToUse.memory,
+    pricePerUnit: awsInstanceTypeToUse.pricePerUnit,
+    awsVmSubTotal: awsVmSubTotal,
+    awsStorageSubTotal: awsStorageSubTotal,
+    awsTotal: awsTotal,
+};
+
+export default objToExport;
 
